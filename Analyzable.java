e5b2db94a6bdd64b93111b89f85b456115890166
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-11-18 14:53:53 -0800

    Create Analyzable.java

---
+++ Analyzable.java
@@ -0,0 +1,10 @@
+// File: Analyzable.java
+// Programmer: Jennifer King
+// Date: 10/7/2018
+
+public interface Analyzable 
+{
+	double getAverage();
+	GradedActivity getHighest();
+	GradedActivity getLowest();
+}
